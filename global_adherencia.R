bce2c7d161e6ae656a476d4f22eda563e9cd8462
jrealgatius <jordireal@gmail.com>
2020-02-24 20:29:49 +0100

    ACtualització

--- global_adherencia.R
+++ global_adherencia.R
@@ -54,6 +54,60 @@ truncament_farmacs<-function(dt_historic=mostra_facturats_post2,data_inferior="d
     select(-min,-max)
 }
 
-
+generar_mapa<-function(gap=1,set_seed=9,dadesprescripcio=dt_prescrits) {
+  # gap<-0 # gap: gap=45
+  # set_seed<-3  # set_seed=12
+  
+  # 0. Mostrejar id's
+  set.seed(set_seed)
+  
+  prescrits_id<-dadesprescripcio %>% semi_join(dt_ids %>% sample_n(1),by="idp")
+  facturats_id<-dt_facturats %>% semi_join(prescrits_id,by="idp")
+  
+  # 2.agregar solapaments tant de prescrits com facturats
+  prescrits_id<-agregar_solapaments_gaps(dt=prescrits_id,id="idp",datainici =
+                                           "dat",datafinal = "dbaixa",gap=gap)
+  facturats_id<-facturats_id %>%
+    mutate(dat=lubridate::ymd(paste0(as.character(dat),"15")),dbaixa=dat+(30*env)) %>%
+    agregar_solapaments_gaps(id="idp",datainici = "dat",datafinal = "dbaixa",gap=gap)
+  # 2.2. Juntar prescrits + dispensats
+  total_id<- mutate(facturats_id,tipus="Facturats") %>% rbind(mutate(prescrits_id,tipus="Prescrits"))
+  total_id<-total_id %>% unite(id_kk,c(idp,tipus),remove = F)
+  # Mapejar
+  mapa<-MAP_ggplot(dades=total_id,datainicial = "dat",datafinal =
+                     "dbaixa",id="id_kk",grup_linea = "tipus",grup_color = "tipus")
+  
+  # sumar temps acumulats
+  temps_per_id<-total_id %>% mutate(temps=dbaixa-dat) %>% group_by(idp,tipus) %>%
+    summarise(Temps=sum(temps))
+  # Generar taulaMPR
+  
+  # Si no existeix dispensat = 0
+  temps_dispensat<-temps_per_id$Temps[1] %>% as.numeric()
+  temps_prescrit<-temps_per_id$Temps[2] %>% as.numeric()
+  
+  if (is.na(temps_prescrit)) {
+    temps_prescrit<-temps_dispensat
+    temps_dispensat<-0}
+  
+  tableMPR<-temps_dispensat/temps_prescrit %>%
+    as_tibble() %>%
+    select(MPR=value)
+  # 3. Posar-ho en una taula
+  tableMPR<-tableMPR %>%
+    mutate(D=temps_dispensat,
+           P=temps_prescrit,
+           Adherence=if_else(MPR>0.8,"Good: >=0.8","Low: <0.8")) %>%
+    transmute(D,P,MPR=round(MPR,3),Adherence)
+  
+  subtitul<-paste0("MPR: ",round(tableMPR$MPR,3)," del subjecte: ",total_id$idp[1])
+  
+  
+  mapa+labs(x="Temps",
+            y="Subjecte",
+            title="Seguiment fàrmacs",
+            subtitle=subtitul)+
+    annotation_custom(gridExtra::tableGrob(tableMPR))
+}
 
 
